5dd025b4f6aeacd95ceaf784a393d744d065b057
hanyu <531868219@qq.com>
2020-07-04 16:55:56 +0800

    test

--- service/model/userDao.go
+++ service/model/userDao.go
@@ -1,12 +1,11 @@
 package model
 
 import (
-	"fmt",
-	""
+	"github.com/gomodule/redigo/redis"
 )
 
 // 定义一个userDao结构体
 // 完成对user结构体的各种操作
 type UserDao struct {
-	pool *redis.pool
+	pool *redis.Pool
 }
